7387cabbfd6ad8653a12bb91c0bac8d21c246d57
mahdib619 <mahdib619@gmail.com>
2024-05-25 15:25:28 +0330

    Implement get all shows

---
+++ src/ShowTrack.Web/Controllers/ShowsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShowTrack.Web.Models.Dtos;
+using ShowTrack.Web.Services;
+using System.Security.Claims;
+
+namespace ShowTrack.Web.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class ShowsController(IShowService showService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyCollection<ReadShowDto>>> GetAllShows()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        var shows = await showService.GetAllUserShows(userId);
+        return Ok(shows);
+    }
+}
