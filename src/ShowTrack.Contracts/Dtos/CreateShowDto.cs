9dfe7ecce81983640ab219e1118ea817e64c455d
mahdib619 <mahdib619@gmail.com>
2024-06-11 11:48:12 +0330

    Implement adding shows

--- src/ShowTrack.Contracts/Dtos/CreateShowDto.cs
+++ src/ShowTrack.Contracts/Dtos/CreateShowDto.cs
@@ -4,9 +4,11 @@ namespace ShowTrack.Contracts.Dtos;
 
 public sealed class CreateShowDto
 {
-    public required string Title { get; init; }
+    public required string Title { get; set; }
     public required string UserId { get; set; }
-    public required string CurrentSeason { get; init; }
+    public required string CurrentSeason { get; set; }
+
+    public static CreateShowDto New() => new() { UserId = "", CurrentSeason = "", Title = "" };
 
     public Show ToEntity() => new()
     {
