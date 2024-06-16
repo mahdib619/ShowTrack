be84713ca6783c97ea05db561bd86dbc88bbf6d9
mahdib619 <mahdib619@gmail.com>
2024-06-16 07:36:33 +0330

    implement getting current user

---
+++ src/ShowTrack.Contracts/Dtos/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace ShowTrack.Contracts.Dtos;
+
+public sealed class UserInfo
+{
+    public bool IsAuthenticated { get; init; }
+    public string? UserName { get; set; }
+    public Dictionary<string, string>? Claims { get; init; }
+}
---
+++ src/ShowTrack.Web/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShowTrack.Contracts.Dtos;
+
+namespace ShowTrack.Web.Controllers;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<UserInfo> UserInfo()
+    {
+        var isAuthenticated = User.Identity?.IsAuthenticated ?? false;
+        return Ok(new UserInfo
+        {
+            IsAuthenticated = isAuthenticated,
+            UserName = User.Identity?.Name,
+            Claims = isAuthenticated ? User.Claims.ToDictionary(c => c.Type, c => c.Value) : null
+        });
+    }
+}
