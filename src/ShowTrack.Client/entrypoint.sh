967d2651e46d51747a9967d6f1d07e6edfb24b06
mahdib619 <mahdib619@gmail.com>
2024-06-18 09:12:53 +0330

    Implement updating client base url by env vars

---
+++ src/ShowTrack.Client/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Check if BASE_URL environment variable is set
+if [ -z "$BASE_URL" ]; then
+  echo "BASE_URL is not set. Using default value."
+else
+  echo "Updating BaseUrl to $BASE_URL in appsettings.json"
+
+  # Update the BaseUrl property in appsettings.json
+  jq --arg baseUrl "$BASE_URL" '.BaseAddress = $baseUrl' /app/wwwroot/appsettings.json > /app/wwwroot/appsettings.tmp.json && mv /app/wwwroot/appsettings.tmp.json /usr/share/nginx/html/appsettings.json
+fi
+
+nginx -g "daemon off;"
