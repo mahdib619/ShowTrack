0c399c848b150f0907acecb9bb372d9df8694c79
mahdib619 <mahdib619@gmail.com>
2024-06-23 10:20:14 +0330

    Implement nav menu profile section

---
+++ src/ShowTrack.Client/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace ShowTrack.Client.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string? GetEmail(this ClaimsPrincipal principal)
+    {
+        return principal.Identity?.IsAuthenticated is true ? principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value : null;
+    }
+}
