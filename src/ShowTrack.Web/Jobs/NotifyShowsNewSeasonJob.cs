5c12de29d4994bae5b945b7992bbcb4db72c91e0
mahdib619 <mahdib619@gmail.com>
2024-07-01 07:49:26 +0330

    Fix sending email

--- src/ShowTrack.Web/Jobs/NotifyShowsNewSeasonJob.cs
+++ src/ShowTrack.Web/Jobs/NotifyShowsNewSeasonJob.cs
@@ -12,16 +12,14 @@ public sealed class NotifyShowsNewSeasonJob(IShowService showService, IEmailServ
     {
         var todayShows = await showService.GetTodayShows();
 
-        await Task.WhenAll(todayShows.Select(Notify));
-
-        await showService.DeleteSchedulesAndUpdateShowSeason(todayShows.Select(s => s.Schedule!.Id).ToArray());
-
-        async Task Notify(Show show)
+        foreach (var show in todayShows)
         {
             if (show.User?.Email is not null)
             {
                 await emailService.Send(show.User.Email, "New season notification", string.Format(NOTIFY_SHOW_TEMPLATE, show.Schedule!.Season, show.Title));
             }
         }
+
+        await showService.DeleteSchedulesAndUpdateShowSeason(todayShows.Select(s => s.Schedule!.Id).ToArray());
     }
 }
