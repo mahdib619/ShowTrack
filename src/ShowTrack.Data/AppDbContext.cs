6282c84ba9f3e3e82b95fa94115ec42666080ee6
mahdib619 <mahdib619@gmail.com>
2024-07-02 12:49:43 +0330

    save datetimes as utc in postgressql

--- src/ShowTrack.Data/AppDbContext.cs
+++ src/ShowTrack.Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShowTrack.Domain.Entities;
 
 namespace ShowTrack.Data;
@@ -17,5 +18,41 @@ public sealed class AppDbContext : IdentityDbContext
     {
         builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
         base.OnModelCreating(builder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        if (Database.ProviderName?.Contains("PostgreSQL",StringComparison.CurrentCultureIgnoreCase) is true)
+        {
+            configurationBuilder.Properties<DateTime>().HaveConversion<DateTimeUtcConverter>();
+
+        }
+    }
+}
+
+file class DateTimeUtcConverter : ValueConverter<DateTime, DateTime>
+{
+    public DateTimeUtcConverter() : base(dt => ToProvider(dt), dt => FromProvider(dt))
+    {
+    }
+
+    private static DateTime ToProvider(DateTime dateTime)
+    {
+        if (dateTime.Kind is not DateTimeKind.Utc)
+        {
+            dateTime = dateTime.ToUniversalTime();
+        }
+
+        return dateTime;
+    }
+
+    private static DateTime FromProvider(DateTime dateTime)
+    {
+        if (dateTime.Kind is not DateTimeKind.Local)
+        {
+            dateTime = dateTime.ToLocalTime();
+        }
+
+        return dateTime;
     }
 }
